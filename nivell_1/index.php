b01fe4e3137acf256bf8618ec6414cdda3c2523a
Carla Velasco <carlavelasco15@gmail.com>
2022-01-17 12:22:49 +0100

    Sprint 1 - Tema 3 - Nivell 1 - Exercici 4

--- nivell_1/index.php
+++ nivell_1/index.php
@@ -38,13 +38,78 @@
     echo "<p>Concatenar: $hola $curs</p>";
     echo "<br>";
 
-     /* EXERCICI 3 */
+
+    /* EXERCICI 3 */
 
      echo "<h5>EXERCICI 3</h5>";
 
      define("NOM", "Carla");
      echo "<h1><b>" . NOM . " </b></h1>";
      echo "<br>";
+
+
+    /* EXERCICI 4 */
+
+    echo "<h5>EXERCICI 4</h5>";
+
+    $x = 11;
+    $y = 3;
+    $n = 3.56;
+    $m = 1.17;
+
+    echo "<p>Variable X: $x</p>";
+    echo "<p>Variable Y: $y</p>";
+    echo "<p>Variable N: $n</p>";
+    echo "<p>Variable M: $m</p>";
+    echo "<br>";
+
+    echo "<p><b>Variables X i Y</b></p>";
+    $suma = $x + $y;
+    $resta = $x - $y;
+    $mult = $x * $y;
+    $residu = $x % $y;
+    echo "<p>Suma: $x + $y = $suma</p>";
+    echo "<p>Resta: $x - $y = $resta</p>";
+    echo "<p>Multiplicació: $x x $y = $mult</p>";
+    echo "<p>Módul: $x % $y = $residu</p>";
+
+    echo "<br>";
+
+    echo "<p><b>Variables N i M</b></p>";
+    $suma = $n + $m;
+    $resta = $n - $m;
+    $mult = $n * $m;
+    $residu = $n % $m;
+    echo "<p>Suma: $n + $m = $suma</p>";
+    echo "<p>Resta: $n - $m = $resta</p>";
+    echo "<p>Multiplicació: $n x $m = $mult</p>";
+    echo "<p>Módul: $n % $m = $residu</p>";
+
+    echo "<br>";
+
+    echo "<p><b>Totes les variables</b></p>";
+    function doble($num) {
+        return $num * 2;
+    }
+    $sum_tot = $x + $y + $n + $m;
+    $mult_tot = $x * $y * $n * $m;
+
+
+    echo "<p>Doble de X: " . doble($x) . "</p>";
+    echo "<p>Doble de Y: " . doble($y) . "</p>";
+    echo "<p>Doble de N: " . doble($n) . "</p>";
+    echo "<p>Doble de M: " . doble($m) . "</p>";
+    echo "<p>Suma de totes les variables: $x + $y + $n + $m = $sum_tot</p>";
+    echo "<p>Multiplicació de totes les variables: $x x $y x $n x $m = $mult_tot</p>";
+
+
+
+
+
+
+
+
+    echo "<br>";
     ?>
 </body>
 </html>
